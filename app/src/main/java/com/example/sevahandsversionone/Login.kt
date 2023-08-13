228671669ca90b161e091c27f2a83d59886557c1
ST10084431 <130999115+ST10084431@users.noreply.github.com>
2023-08-13 19:53:08 +0200

    Update Login.kt

--- app/src/main/java/com/example/sevahandsversionone/Login.kt
+++ app/src/main/java/com/example/sevahandsversionone/Login.kt
@@ -1,7 +1,7 @@
 package com.example.sevahandsversionone
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
@@ -13,82 +13,83 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
-    private lateinit var userEmail: EditText
-    private lateinit var Password: EditText
-    private lateinit var btnlogin: Button
-    private lateinit var mAuth: FirebaseAuth
+  private lateinit var userEmail: EditText
+  private lateinit var Password: EditText
+  private lateinit var btnlogin: Button
+  private lateinit var mAuth: FirebaseAuth
 
-    override  fun onCreate(savedInstanceState: Bundle?){
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
 
+    // Initialize views and Firebase Authentication
+    userEmail = findViewById(R.id.userEmail)
+    Password = findViewById(R.id.password)
+    btnlogin = findViewById(R.id.btnLogin)
+    mAuth = FirebaseAuth.getInstance()
 
-      userEmail = findViewById(R.id.userEmail)
-      Password = findViewById(R.id.password)
-      btnlogin = findViewById(R.id.btnLogin)
-      mAuth = FirebaseAuth.getInstance()
-      btnlogin.setOnClickListener{
-        loginUser()
-      }
-
-      //when the user clicks on the create account text - they will be routed to the regindiv page
-      val textViewClickable = findViewById<TextView>(R.id.reglink)
-      textViewClickable.setOnClickListener {
-        // Create an Intent to navigate to the SecondActivity
-        val intent = Intent(this@Login, Regindiv::class.java)
-        startActivity(intent)
-      }
+    // Set click listener for login button
+    btnlogin.setOnClickListener {
+      loginUser()
     }
-  private fun loginUser() {
-    {
-      val intent = Intent(this, Home::class.java)
+
+    // Set click listener for "Create Account" text
+    val textViewClickable = findViewById<TextView>(R.id.reglink)
+    textViewClickable.setOnClickListener {
+      // Navigate to registration activity
+      val intent = Intent(this@Login, Regindiv::class.java)
       startActivity(intent)
-      finish()
     }
+  }
 
+  // Function to handle login process
+  private fun loginUser() {
     try {
       val email = userEmail.text.toString().trim()
       val password = Password.text.toString().trim()
 
-      if (TextUtils.isEmpty(email)){
+      // Validate email and password
+      if (TextUtils.isEmpty(email)) {
         Toast.makeText(this, "Please enter an email address!", Toast.LENGTH_SHORT).show()
         userEmail.requestFocus()
         return
       }
       if (TextUtils.isEmpty(password)) {
-        Toast.makeText(this, "Please enter a password!",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Please enter a password!", Toast.LENGTH_SHORT).show()
         Password.requestFocus()
         return
       }
-      mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-        if (task.isSuccessful) {val intent = Intent (this@Login, Task::class.java)
-          startActivity(intent)
 
-          Toast.makeText(this,"Login Successful!", Toast.LENGTH_SHORT).show()
+      // Sign in user with Firebase Authentication
+      mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+          // Login successful, navigate to Home activity
+          val intent = Intent(this@Login, Home::class.java)
+          startActivity(intent)
+          finish()
+        } else {
+          // Login unsuccessful, display error message
+          Toast.makeText(this, "Login Unsuccessful! Please try again.", Toast.LENGTH_SHORT).show()
           userEmail.setText("")
           Password.setText("")
           userEmail.requestFocus()
-      }else {
-        Toast.makeText(this,"Login Unsucessful! Please try again." , Toast.LENGTH_SHORT).show()
-          userEmail.setText("")
-          Password.setText("")
-          userEmail.setText("")
         }
+      }
+    } catch (eer: Exception) {
+      // Handle and display any exceptions that occur during login
+      Toast.makeText(this, "Error Occurred: " + eer.message, Toast.LENGTH_SHORT).show()
     }
-  } catch (eer: Exception) {
-  Toast.makeText(this, "Error Occured:" + eer.message, Toast.LENGTH_SHORT).show()
   }
-  }
-  fun reg(view: View)
-  {
-    val intent = Intent (this, Regindiv::class.java)
+
+  // Function to handle "Create Account" button click
+  fun reg(view: View) {
+    val intent = Intent(this, Regindiv::class.java)
     startActivity(intent)
   }
-  fun clearbtn (view: View)
-  {
+
+  // Function to clear input fields
+  fun clearbtn(view: View) {
     userEmail.text.clear()
     Password.text.clear()
   }
-
-
 }
