00aa8f625d9065d45fe626cab5a48d6ba5fa04f5
revalireddy <98344546+revalireddy@users.noreply.github.com>
2023-09-01 08:26:34 +0200

    added admin pages

---
+++ app/src/main/java/com/example/sevahandsversionone/admin_events.kt
@@ -0,0 +1,11 @@
+package com.example.sevahandsversionone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class admin_events : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_events)
+    }
+}
