983f4d353e60bead8155fdf799aa7acc8862617f
revalireddy <98344546+revalireddy@users.noreply.github.com>
2023-09-01 09:07:32 +0200

    Update VolunteerUser.kt

--- app/src/main/java/com/example/sevahandsversionone/ui/VolunteerUser.kt
+++ app/src/main/java/com/example/sevahandsversionone/ui/VolunteerUser.kt
@@ -2,6 +2,7 @@ package com.example.sevahandsversionone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sevahandsversionone.R
 
 class VolunteerUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
