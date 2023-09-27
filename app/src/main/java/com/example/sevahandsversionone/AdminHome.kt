544a30e31cbf471e275170d246e6e59d75ff5e03
revalireddy <98344546+revalireddy@users.noreply.github.com>
2023-09-27 22:00:08 +0200

    Admin nav bar b-end
    -admin user is able to navigate to different pages using the nav bar

--- app/src/main/java/com/example/sevahandsversionone/AdminHome.kt
+++ app/src/main/java/com/example/sevahandsversionone/AdminHome.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AdminHome : AppCompatActivity() {
     private lateinit var viewEventButton: Button // Declare the button variable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.selectedItemId = R.id.navigation_home
 
         // Initialize the button using findViewById
         viewEventButton = findViewById(R.id.viewEvent)
@@ -20,5 +24,64 @@ class AdminHome : AppCompatActivity() {
             val intent = Intent(this, admin_events::class.java)
             startActivity(intent)
         }
+
+
+    bottomNavigationView.setOnItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@setOnItemSelectedListener true
+            }
+            R.id.navigation_events -> {
+                startActivity(Intent(applicationContext, admin_events::class.java))
+                overridePendingTransition(R.transition.right, R.transition.left)
+                finish()
+                return@setOnItemSelectedListener true
+            }
+            R.id.navigation_volunteers -> {
+                startActivity(Intent(applicationContext, admin_volunteers::class.java))
+                overridePendingTransition(R.transition.right, R.transition.left)
+                finish()
+                return@setOnItemSelectedListener true
+            }
+
+            R.id.navigation_users -> {
+                startActivity(Intent(applicationContext, admin_users::class.java))
+                overridePendingTransition(R.transition.right, R.transition.left)
+                finish()
+                return@setOnItemSelectedListener true
+            }
+        }
+        return@setOnItemSelectedListener false
+    }
+}
+
+private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    // Start AlgorithmActivity
+                    val intent = Intent(this, Home::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_volunteers -> {
+                    // Start CourseActivity
+                    val intent = Intent(this, admin_volunteers::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_users -> {
+                    // Start ProfileActivity
+                    val intent = Intent(this, admin_users::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_events -> {
+                    // Start ProfileActivity
+                    val intent = Intent(this, admin_events::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> return@OnNavigationItemSelectedListener false
+            }
     }
 }
