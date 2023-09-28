3ad495153272a66effe6031ddfd619cc3915409f
revalireddy <98344546+revalireddy@users.noreply.github.com>
2023-09-28 21:58:35 +0200

    Contact us page - social accounts
    -users can now click on image button (social media button) and Seva Hands social account will open

--- app/src/main/java/com/example/sevahandsversionone/Contact.kt
+++ app/src/main/java/com/example/sevahandsversionone/Contact.kt
@@ -1,7 +1,9 @@
 package com.example.sevahandsversionone
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,21 +15,17 @@ class Contact : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_contact -> {
                     return@setOnItemSelectedListener true
                 }
-
                 R.id.navigation_dashboard -> {
                     startActivity(Intent(applicationContext, DashBoard::class.java))
                     overridePendingTransition(R.transition.right, R.transition.left)
                     finish()
                     return@setOnItemSelectedListener true
                 }
-
-
                 R.id.navigation_home -> {
                     startActivity(Intent(applicationContext, Home::class.java))
                     overridePendingTransition(R.transition.right, R.transition.left)
@@ -35,10 +33,37 @@ class Contact : AppCompatActivity() {
                     return@setOnItemSelectedListener true
                 }
             }
+            return@setOnItemSelectedListener false
+        }
 
-                return@setOnItemSelectedListener false
+        // Find the ImageButtons
+        val instagramButton = findViewById<ImageButton>(R.id.InstagramBtn)
+        val facebookButton = findViewById<ImageButton>(R.id.FaceBookBtn)
+        val TickTokBtn = findViewById<ImageButton>(R.id.TickTokBtn)
+        val linkedin = findViewById<ImageButton>(R.id.linkedin)
 
+
+        // Set click listeners for Instagram and Facebook buttons
+        instagramButton.setOnClickListener {
+            openLink("https://www.instagram.com/sevahands/")
+        }
+
+        facebookButton.setOnClickListener {
+            openLink("https://www.facebook.com/sevahands108/")
+        }
+
+        TickTokBtn.setOnClickListener {
+            openLink("hhttps://www.tiktok.com/@sevahands108")
+        }
+
+        linkedin.setOnClickListener {
+            openLink("https://za.linkedin.com/company/seva-hands-enterprise")
         }
     }
-}
 
+    private fun openLink(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+}
