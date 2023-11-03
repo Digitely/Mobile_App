76e4c1555653696340bd145ed7e0f425d152e81b
ST10084431 <130999115+ST10084431@users.noreply.github.com>
2023-11-03 12:20:18 +0200

    counter

---
+++ app/src/main/java/com/example/sevahandsversionone/Counter.kt
@@ -0,0 +1,11 @@
+package com.example.sevahandsversionone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Counter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+    }
+}
